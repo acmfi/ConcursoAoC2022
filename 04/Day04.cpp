d2912e258a5038703243f6ace7eec2b779079345
Pantareje-UPM <sergio.gil.atienza@alumnos.upm.es>
2022-12-05 19:33:31 +0100

    Día 4 parte 1

--- 04/Day04.cpp
+++ 04/Day04.cpp
@@ -1,20 +1,42 @@
-// Día 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+
+using uint = unsigned int;
+constexpr auto PATH = "input"; // input, sample
+
+uint getAllContained() {
+    std::ifstream input;
+    input.open(PATH);
+
+    uint totalContained = 0;
+
+    while (!input.eof())
+    {
+        uint elfOneStart;
+        uint elfOneEnd;
+        uint elfTwoStart;
+        uint elfTwoEnd;
+
+        input >> elfOneStart;
+        input.ignore(1);
+        input >> elfOneEnd;
+        input.ignore(1);
+        input >> elfTwoStart;
+        input.ignore(1);
+        input >> elfTwoEnd;
+
+        if (elfOneStart > elfTwoStart && elfOneEnd <= elfTwoEnd)
+            totalContained++;
+        else if (elfOneStart < elfTwoStart && elfOneEnd >= elfTwoEnd)
+            totalContained++;
+        else if (elfOneStart == elfTwoStart)
+            totalContained++;
+    }
+
+    return totalContained;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "El total de rangos contenidos es " << getAllContained();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
