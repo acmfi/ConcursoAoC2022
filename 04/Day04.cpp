9e6ca8a8d8012e7ee96d5d52d2f70422ecd2616f
Pantareje-UPM <sergio.gil.atienza@alumnos.upm.es>
2022-12-05 20:22:16 +0100

    Día 4 parte 2

--- 04/Day04.cpp
+++ 04/Day04.cpp
@@ -4,39 +4,75 @@
 using uint = unsigned int;
 constexpr auto PATH = "input"; // input, sample
 
-uint getAllContained() {
-    std::ifstream input;
-    input.open(PATH);
+bool loadRange(std::ifstream& input, uint& r1Start, uint& r1End, uint& r2Start, uint& r2End)
+{
+	// Sólo comprobamos el primero
+	if (!(input >> r1Start))
+		return false;
+	input.ignore(1);
+	input >> r1End;
+	input.ignore(1);
+	input >> r2Start;
+	input.ignore(1);
+	input >> r2End;
+	input.ignore(1);
 
-    uint totalContained = 0;
+	return true;
+}
 
-    while (!input.eof())
-    {
-        uint elfOneStart;
-        uint elfOneEnd;
-        uint elfTwoStart;
-        uint elfTwoEnd;
+uint getAllContained()
+{
+	std::ifstream input;
+	input.open(PATH);
 
-        input >> elfOneStart;
-        input.ignore(1);
-        input >> elfOneEnd;
-        input.ignore(1);
-        input >> elfTwoStart;
-        input.ignore(1);
-        input >> elfTwoEnd;
+	uint totalContained = 0;
 
-        if (elfOneStart > elfTwoStart && elfOneEnd <= elfTwoEnd)
-            totalContained++;
-        else if (elfOneStart < elfTwoStart && elfOneEnd >= elfTwoEnd)
-            totalContained++;
-        else if (elfOneStart == elfTwoStart)
-            totalContained++;
-    }
+	uint elfOneStart;
+	uint elfOneEnd;
+	uint elfTwoStart;
+	uint elfTwoEnd;
 
-    return totalContained;
+	while (loadRange(input, elfOneStart, elfOneEnd, elfTwoStart, elfTwoEnd))
+	{
+		if ((elfOneStart >= elfTwoStart && elfOneEnd <= elfTwoEnd) ||
+			(elfOneStart <= elfTwoStart && elfOneEnd >= elfTwoEnd))
+		{
+			totalContained++;
+		}
+	}
+
+	return totalContained;
+}
+
+uint getAllOverlapped()
+{
+	std::ifstream input;
+	input.open(PATH);
+
+	uint totalOverlap = 0;
+
+	uint elfOneStart;
+	uint elfOneEnd;
+	uint elfTwoStart;
+	uint elfTwoEnd;
+
+	while (loadRange(input, elfOneStart, elfOneEnd, elfTwoStart, elfTwoEnd))
+	{
+		if ((elfOneStart <= elfTwoStart && elfTwoStart <= elfOneEnd) ||
+			(elfTwoStart <= elfOneStart && elfOneStart <= elfTwoEnd))
+		{
+			totalOverlap++;
+		}
+	}
+
+	return totalOverlap;
 }
 
 int main()
 {
-    std::cout << "El total de rangos contenidos es " << getAllContained();
+	std::cout << "El total de rangos contenidos es " << getAllContained() << '\n';
+	std::cout << "El total de rangos que se superponen es " << getAllOverlapped() << '\n';
 }
+
+// Primera parte: 444
+// Segunda parte: 801
