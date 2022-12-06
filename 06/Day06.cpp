2925ee22aabd70b3f2b451684036333a324a3964
Pantareje-UPM <sergio.gil.atienza@alumnos.upm.es>
2022-12-06 15:46:09 +0100

    Parte 2

--- 06/Day06.cpp
+++ 06/Day06.cpp
@@ -6,20 +6,34 @@
 using uint = unsigned int;
 constexpr auto PATH = "input"; // input, sample
 
+uint findDistinctCharacters(const std::string& line, const uint length)
+{
+    for (uint i = 0; i < line.length() - length; i++)
+    {
+        std::unordered_set<char> prevChars;
+
+        for (uint j = 0; j < length; j++)
+        {
+            prevChars.insert(line.at(i + j));
+        }
+
+        if (prevChars.size() == length)
+            return (i + length);
+    }
+
+    return -1;
+}
+
 int main()
 {
     std::ifstream input(PATH);
     std::string line;
     std::getline(input, line);
-    for (uint i = 0; i < line.length() - 3; i++)
-    {
-        std::unordered_set<char> prevChars;
-
-        prevChars.insert(line.at(i));
-        prevChars.insert(line.at(i + 1));
-        prevChars.insert(line.at(i + 2));
-        prevChars.insert(line.at(i + 3));
-        if (prevChars.size() == 4)
-            return (i + 3 + 1);
-    }
+    std::cout << "1 - Para encontrar el inicio del paquete son necesarios "
+        << findDistinctCharacters(line, 4) << " caracteres\n";
+    std::cout << "1 - Para encontrar el inicio del mensaje son necesarios "
+        << findDistinctCharacters(line, 14) << " caracteres\n";
 }
+
+// Primer marcador: 1480
+// Segundo marcador: 2746
